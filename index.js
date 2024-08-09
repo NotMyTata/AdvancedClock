e9e39c07e5caebfacfd8b8110c3323fd7c79148f
Sasmita <tata.wiropati@gmail.com>
2024-08-09 20:39:16 +0700

    Added new files

---
+++ index.js
@@ -0,0 +1,11 @@
+function changeToClock(){
+    document.getElementById('content').src = 'clock/clock.html';
+}
+
+function changeToStopwatch(){
+    document.getElementById('content').src = 'stopwatch/stopwatch.html';
+}
+
+function changeToTimer(){
+    document.getElementById('content').src = 'timer/timer.html';
+}
